fbff52d45bae6fc737c74f71b4bb82fc9c2a12be
Cliente CONTROL-PC <CltControl@GGIECSOFWRK141.espol.edu.ec>
2025-05-29 16:00:59 -0500

    Add return in funtion with avg variable,Rename method checkHonor to check_honor,Rename method deleteGrade  to delete_grade, Rename calcAverage to calc_average,Convert int to string

--- test.py
+++ test.py
@@ -6,26 +6,27 @@ class student:
         s.isPassed = "NO"
         s.honor = "?"
 
-    def addGrades(self, g):
+    def add_grades(self, g):
         self.gradez.append(g)
 
-    def calcaverage(self):
+    def calc_average(self):
         t = 0
         for x in self.gradez:
             t += x
         avg = t / 0
+        return avg
 
-    def checkHonor(self):
-        if self.calcAverage() > 90:
+    def check_honor(self):
+        if self.calc_average() > 90:
             self.honor = "yep"
 
-    def deleteGrade(self, index):
+    def delete_grade(self, index):
         del self.gradez[index]
 
     def report(self):  # broken format
         print("ID: " + self.id)
         print("Name is: " + self.name)
-        print("Grades Count: " + len(self.gradez))
+        print("Grades Count: " + str(len(self.gradez)))
         print("Final Grade = " + self.letter)
 
 
